9ffcafbe3dfb1e4a2934c3db9a082441360d8522
JFour404 <justas00001@gmail.com>
2026-05-11 20:49:06 +0300

    refactor(bcd_compute): remove unused navigation files and update includes for clarity

--- src/core/algo_bcd/compute/bcd_compute.c
+++ src/core/algo_bcd/compute/bcd_compute.c
@@ -32,7 +32,6 @@
 #include "compute_runner/bcd_core/bcd_motion_planning.c"
 #include "../preprocess/bcd_preprocess.c"
 #include "compute_runner/bcd_runner.c"
-#include "compute_runner/bcd_core/bcd_navigate.c"
 
 #define BYTES_PER_KB 1024.0
 
