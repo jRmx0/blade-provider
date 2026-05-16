2fc36b7e360b02c68f55de49cbcc81dd5c91555f
JFour404 <justas00001@gmail.com>
2026-05-17 00:55:42 +0300

    feat(cstar): refactor request validation and error handling in C* compute pipeline

--- src/core/algo_cstar/cstar.c
+++ src/core/algo_cstar/cstar.c
@@ -1,7 +1,29 @@
 #include "cstar.h"
+#include "../../../dependencies/cJSON/cJSON.h"
+#include "check/cstar_check.h"
+#include "parser/cstar_parser.h"
 
+#include "check/cstar_check.c"
+#include "parser/cstar_parser.c"
 #include "metadata/cstar_metadata.c"
 #include "compute/cstar_compute.c"
+
+static char *cstar_create_entrypoint_error_json(const char *code, const char *message)
+{
+    cJSON *response = cJSON_CreateObject();
+    if (response == NULL)
+    {
+        return NULL;
+    }
+
+    cJSON_AddStringToObject(response, "status", "error");
+    cJSON_AddStringToObject(response, "code", code != NULL ? code : "cstar_error");
+    cJSON_AddStringToObject(response, "message", message != NULL ? message : "C* compute pipeline failed.");
+
+    char *json = cJSON_PrintUnformatted(response);
+    cJSON_Delete(response);
+    return json;
+}
 
 char *cstar_get_metadata_json(void)
 {
@@ -10,5 +32,26 @@ char *cstar_get_metadata_json(void)
 
 char *cstar_compute(const char *input_environment_json)
 {
-    return cstar_run_compute(input_environment_json);
+    input_environment_t environment;
+    cstar_check_result_t check_result;
+
+    if (!cstar_validate_request_json(input_environment_json, &check_result))
+    {
+        return cstar_create_entrypoint_error_json(check_result.code, check_result.message);
+    }
+
+    if (!cstar_parse_request_json(input_environment_json, &environment, &check_result))
+    {
+        return cstar_create_entrypoint_error_json(check_result.code, check_result.message);
+    }
+
+    char *json = cstar_run_compute(&environment);
+    cstar_parser_free_environment(&environment);
+
+    if (json == NULL)
+    {
+        return cstar_create_entrypoint_error_json("allocation_failed", "C* compute pipeline allocation failed.");
+    }
+
+    return json;
 }
